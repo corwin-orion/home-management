277242be2fcff22bc7a6af50011103285dd080f5
corwin <corwinschmidt@gmail.com>
2024-07-31 21:54:32 -0500

    Add session context

--- app/households.tsx
+++ app/households.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-native';
+import { Button, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { FIREBASE_AUTH, FIRESTORE } from '@/FirebaseConfig';
 import { router } from 'expo-router';
 import PageContainer from '@/components/PageContainer';
@@ -7,9 +7,12 @@ import { collection, query, where, getDocs } from 'firebase/firestore';
 import { Household } from '@/constants/Types';
 import { ThemedText } from '@/components/ThemedText';
 import MenuItem from '@/components/MenuItem';
+import { Feather, Ionicons } from '@expo/vector-icons';
+import { useSession } from '@/contexts/SessionContext';
 
 export default function Login() {
   const [households, setHouseholds] = useState<Household[]>([]);
+  const { user, setUser } = useSession();
 
   useEffect(() => {
     const getHouseholds = async () => {
@@ -34,10 +37,23 @@ export default function Login() {
 
   return (
     <PageContainer style={{ gap: 10 }}>
-      <Button title='Log out' onPress={() => {
-        FIREBASE_AUTH.signOut();
-        router.navigate('/');
-      }} />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => {
+          FIREBASE_AUTH.signOut();
+          router.navigate('/');
+        }}>
+          <Feather name='log-out' size={24} color='black' />
+        </TouchableOpacity>
+        <View style={styles.nameContainer}>
+          <ThemedText type='subtitle' style={{ color: user?.color }}>{user?.name}</ThemedText>
+          <View style={styles.nameButtonsContainer}>
+            <TouchableOpacity onPress={() => { }}>
+              <Feather name='edit' size={24} color='black' />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View></View>
+      </View>
       <ThemedText type='title' style={{ textAlign: 'center' }}>Select household:</ThemedText>
       {households.map(household => (
         <MenuItem key={household.id}>
@@ -47,3 +63,21 @@ export default function Login() {
     </PageContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  nameButtonsContainer: {
+    flexDirection: 'row',
+    gap: 3,
+  }
+});
---
+++ helpers/getUser.ts
@@ -0,0 +1,24 @@
+import { User } from "@/constants/Types";
+import { FIREBASE_AUTH, FIRESTORE } from "@/FirebaseConfig";
+import { getDoc, doc } from "firebase/firestore";
+
+export async function getUser(): Promise<User | null> {
+  // Get the current user from Firebase Auth
+  const user = FIREBASE_AUTH.currentUser;
+
+  // If there is no user, return null
+  if (!user) {
+    return null;
+  }
+
+  // Get the user document from Firestore
+  const userDoc = await getDoc(doc(FIRESTORE, `users/${user.uid}`));
+
+  // If the user document doesn't exist, return null
+  if (!userDoc.exists()) {
+    return null;
+  }
+
+  // Return the user data
+  return userDoc.data() as User;
+}
